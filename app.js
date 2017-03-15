6f9ebf58abe5cec57b751624ac130a59c4a39f82
shuaicj <shuaicj@gmail.com>
2017-03-15 11:08:51 +0800

    add logging system: winston

--- app.js
+++ app.js
@@ -3,26 +3,30 @@ var config = require('config');
 var express = require('express');
 var validator = require('express-validator');
 var mongoose = require('mongoose');
+var logger = require('winston');
 var bodyParser = require('body-parser');
 var User = require('entity/User');
+
+logger.level = config.LOG_LEVEL;
 
 var app = express();
 
 app.use(bodyParser.json());
 app.use(validator());
 
-console.log('conn', process.env.MONGODB_URI || config.MONGODB_URI);
-mongoose.connect(process.env.MONGODB_URI || config.MONGODB_URI);
+mongoose.connect(config.MONGODB_URI);
+mongoose.Promise = global.Promise;
+
 var db = mongoose.connection;
 db.on('error', function() {
-    console.error('Mongodb connection error! Is it running?');
+    logger.error('Mongodb connection error! Is it running?');
 });
 db.once('open', function() {
-    console.log('Mongodb connection ok.');
+    logger.info('Mongodb connection ok.');
 });
 
 app.use('/', require('route/user.js'));
 
 var server = app.listen(config.PORT, function() {
-    console.log('Server up! Listening at port %s.', config.PORT);
+    logger.info('Server up! Listening at port %s.', config.PORT);
 });
--- config.js
+++ config.js
@@ -1,2 +1,5 @@
-exports.PORT = 8080;
-exports.MONGODB_URI = 'mongodb://hellouserjsu:hellouserjsp@localhost:27017/hellouserjs';
+exports.PORT = process.env.PORT || 8080;
+
+exports.LOG_LEVEL = process.env.LOG_LEVEL || 'debug';
+
+exports.MONGODB_URI = process.env.MONGODB_URI || 'mongodb://hellouserjsu:hellouserjsp@localhost:27017/hellouserjs';
